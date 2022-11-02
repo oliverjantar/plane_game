3d6d64764e41a755c66c8a608f04c296d1a471d1
jiriandras <jiriandras@protonmail.com>
2022-11-02 21:24:39 +0100

    Add ws to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,26 @@
-use warp::Filter;
+use messages::ServerMessage;
+use warp::{ws::WebSocket, Filter};
 mod messages;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
+    let game = warp::path("game")
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| ws.on_upgrade(move |socket| user_connected(socket)));
+
     let status = warp::path!("status").map(move || warp::reply::html("hello"));
 
-    warp::serve(status).run(([0, 0, 0, 0], 3030)).await;
+    let routes = status.or(game);
+
+    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
+}
+
+async fn user_connected(ws: WebSocket) {
+    unimplemented!();
+}
+
+async fn send_msg(msg: ServerMessage) {
+    let buffer = serde_json::to_vec(&msg).unwrap();
 }
--- server/src/messages.rs
+++ server/src/messages.rs
@@ -1,25 +1,27 @@
 use glam::Vec2;
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone)]
+#[derive(Deserialize, Serialize, Clone)]
 pub struct State {
     pub pos: Vec2,
     pub r: f32,
 }
 
-#[derive(Clone)]
+#[derive(Deserialize, Serialize, Clone)]
 pub struct RemoteState {
     pub id: usize,
     pub position: Vec2,
     pub rotation: f32,
 }
 
+#[derive(Deserialize, Serialize)]
 pub enum ServerMessage {
     Welcome(usize),
     GoodBye(usize),
     Update(Vec<RemoteState>),
 }
 
+#[derive(Deserialize, Serialize)]
 pub enum ClientMessage {
     State(State),
 }
