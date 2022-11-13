0d28bc724842a691e90230ef3f8e8b720cc6f7d6
jiriandras <jiriandras@protonmail.com>
2022-11-13 21:06:50 +0100

    Handle user states

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,13 @@
 use futures_util::{FutureExt, StreamExt};
-use messages::ServerMessage;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use tokio::sync::mpsc;
+use messages::{ClientMessage, RemoteState, ServerMessage};
+use std::{
+    collections::HashMap,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+};
+use tokio::sync::{mpsc, RwLock};
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use warp::{
     ws::{Message, WebSocket},
@@ -12,15 +18,29 @@ mod messages;
 
 type OutBoundChannel = mpsc::UnboundedSender<std::result::Result<Message, warp::Error>>;
 
+type Users = Arc<RwLock<HashMap<usize, OutBoundChannel>>>;
+
+type States = Arc<RwLock<HashMap<usize, RemoteState>>>;
+
 static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
+    let users = Users::default();
+    let states = States::default();
+
+    let users = warp::any().map(move || users.clone());
+    let states = warp::any().map(move || states.clone());
+
     let game = warp::path("game")
         .and(warp::ws())
-        .map(|ws: warp::ws::Ws| ws.on_upgrade(move |socket| user_connected(socket)));
+        .and(users)
+        .and(states)
+        .map(|ws: warp::ws::Ws, users, states| {
+            ws.on_upgrade(move |socket| user_connected(socket, users, states))
+        });
 
     let status = warp::path!("status").map(move || warp::reply::html("hello"));
 
@@ -29,7 +49,7 @@ async fn main() {
     warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
 
-async fn user_connected(ws: WebSocket) {
+async fn user_connected(ws: WebSocket, users: Users, states: States) {
     let (ws_sender, mut ws_receiver) = ws.split();
 
     let send_channel = create_send_channel(ws_sender);
@@ -37,19 +57,30 @@ async fn user_connected(ws: WebSocket) {
 
     log::debug!("new user conneted: {}", my_id);
 
+    users.write().await.insert(my_id, send_channel);
+
     while let Some(result) = ws_receiver.next().await {
         let msg = match result {
             Ok(msg) => msg,
             Err(e) => {
-                log::warn!("websocket receive error: '{}'", e);
+                log::warn!("websocket err (id={}) '{}'", my_id, e);
                 break;
             }
         };
 
         log::debug!("user sent message: {:?}", msg);
+
+        if let Some(msg) = parse_message(msg) {
+            user_message(my_id, msg, &states).await;
+        }
     }
 
     log::debug!("user disconnected: {}", my_id);
+
+    users.write().await.remove(&my_id);
+    states.write().await.remove(&my_id);
+
+    broadcast(ServerMessage::GoodBye(my_id), &users).await;
 }
 
 async fn send_msg(tx: &OutBoundChannel, msg: &ServerMessage) {
@@ -81,4 +112,33 @@ async fn send_welcome(out: &OutBoundChannel) -> usize {
     send_msg(out, &states).await;
 
     id
+}
+
+async fn broadcast(msg: ServerMessage, users: &Users) {
+    let users = users.read().await;
+    for (_, tx) in users.iter() {
+        send_msg(tx, &msg).await;
+    }
+}
+
+fn parse_message(msg: Message) -> Option<ClientMessage> {
+    if msg.is_binary() {
+        let msg = msg.into_bytes();
+        serde_json::from_slice::<ClientMessage>(msg.as_slice()).ok()
+    } else {
+        None
+    }
+}
+
+async fn user_message(my_id: usize, msg: ClientMessage, states: &States) {
+    match msg {
+        ClientMessage::State(state) => {
+            let msg = RemoteState {
+                id: my_id,
+                position: state.pos,
+                rotation: state.r,
+            };
+            states.write().await.insert(msg.id, msg);
+        }
+    }
 }
